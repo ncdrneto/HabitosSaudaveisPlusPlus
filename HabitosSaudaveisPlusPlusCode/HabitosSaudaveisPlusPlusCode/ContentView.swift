bcef6a2ec26de4328f0754f5dcfed55385505e37
Newton Neto <ncrneto@gmail.com>
2024-05-15 10:10:56 -0300

    Cria o projeto em código Swift

---
+++ HabitosSaudaveisPlusPlusCode/HabitosSaudaveisPlusPlusCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitosSaudaveisPlusPlusCode
+//
+//  Created by Newton Cardoso da Rocha Neto on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitosSaudaveisPlusPlusCode/HabitosSaudaveisPlusPlusCode/HabitosSaudaveisPlusPlusCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitosSaudaveisPlusPlusCodeApp.swift
+//  HabitosSaudaveisPlusPlusCode
+//
+//  Created by Newton Cardoso da Rocha Neto on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitosSaudaveisPlusPlusCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
